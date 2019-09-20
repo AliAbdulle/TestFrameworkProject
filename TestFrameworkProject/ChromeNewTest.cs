d3a06339d24f64595676907b4b814b0df4ce4bcf
AliAbdulle <alidhobey@gmail.com>
2019-09-19 23:04:02 -0500

    Add new files

---
+++ TestFrameworkProject/ChromeNewTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFrameworkProject
+{
+    [TestFixture]
+    [Parallelizable]
+    [Ignore("Skip test")]
+    class ChromeNewTest
+    {
+        [Test]
+        public void testNews()
+        {
+            Console.WriteLine("Test News");
+        }
+    }
+}
