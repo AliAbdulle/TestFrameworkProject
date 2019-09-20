c79e29916d7ff9e8c794cd71b578eb2d9120d5c8
AliAbdulle <alidhobey@gmail.com>
2019-09-19 22:19:31 -0500

    Added chrome browser

---
+++ TestFrameworkProject/ChromeTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFrameworkProject
+{
+    [TestFixture]
+    public class ChromeTest
+    {
+        [Test]
+        public void TestRecieveMail()
+        {
+            //Selenium code
+            Console.WriteLine("Recieve mail");
+        }
+        [Test]
+        public void SemdMail()
+        {
+            //selenium Code
+            Console.WriteLine("Sending email mail");
+        }
+
+        [SetUp]
+        public void setup()
+        {
+            Console.WriteLine("Set up");
+        }
+       
+    }
+}
