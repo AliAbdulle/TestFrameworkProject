d279c426f1deebb10d6c460fe4c6bd4a63b0f67b
AliAbdulle <alidhobey@gmail.com>
2019-09-19 21:54:46 -0500

    first commit

---
+++ TestFrameworkProject/Simpletest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFrameworkProject
+{
+    [TestFixture]
+    public class Simpletest
+    {
+        [Test]
+        public void testMethod()
+        {
+           Console.WriteLine("Test Method");
+        }
+        [Test]
+        public void testLogin()
+        {
+            Console.WriteLine("Test log in");
+        }
+    }
+}
