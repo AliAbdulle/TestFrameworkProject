2d0477449ab62ad5ee797736a8f54300c109e7bc
AliAbdulle <alidhobey@gmail.com>
2019-09-20 19:34:27 -0500

    BaseClass added

---
+++ TestFrameworkProject/BaseTestAll.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System.Drawing.Imaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFrameworkProject
+{[TestFixture]
+    class BaseTestAll
+    {
+        public static void takeScreenshot(string fileName, IWebDriver driver)
+        {
+            ITakesScreenshot screenshotDriver = driver as ITakesScreenshot;
+            Screenshot screenshot = screenshotDriver.GetScreenshot();
+            screenshot.SaveAsFile(
+                "E:\\report\\" + fileName + ".jpeg", System.Drawing.Imaging.ImageFormat.Gif);
+        }
+    }
+}
