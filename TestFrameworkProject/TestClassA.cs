2e11ccdc7b227a1b3deccd3616f6117bc94d9717
AliAbdulle <alidhobey@gmail.com>
2019-09-20 19:10:21 -0500

    finisished work for selenium C#

--- TestFrameworkProject/TestClassA.cs
+++ TestFrameworkProject/TestClassA.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using AventStack.ExtentReports;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,30 @@ using System.Threading.Tasks;
 
 namespace TestFrameworkProject
 {
+    [TestFixture]
     class TestClassA
     {
+
+        [Test]
+        public void testMethod()
+        {
+            ExtentReports extent = new ExtentReports("E:\\report\\report.html");
+            var test =  extent.StartTest("TestClass A", "Simple Description");
+
+            test.Log(LogStatus.Info, "Validating the download link");
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://www.wseleniumhq.org/";
+            driver.Manage().Window.Maximize();
+
+            IWebElement linkText = driver.FindElement(By.LinkText("Download"));
+            linkText.Click();
+
+            extent.EndTest(test);
+            extent.Flush();
+
+            driver.Quit();
+        
+        }
     }
 }
